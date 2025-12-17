5739b1626a9b49ad8bdfa68d4e05ea8fe65b8bf5
thinhlq170 <thinh.lq170@gmail.com>
2025-12-17 07:39:03 +0700

    [semaphore] Update reader-writer-nostarve

--- threads-sema/reader-writer-nostarve.c
+++ threads-sema/reader-writer-nostarve.c
@@ -7,56 +7,91 @@
 // Your code goes in the structure and functions below
 //
 
-typedef struct __rwlock_t {
+typedef struct __rwlock_t
+{
+    int readers;
+    sem_t lock;
+    sem_t writelock;
+    sem_t turnstile;
 } rwlock_t;
 
-
-void rwlock_init(rwlock_t *rw) {
+void rwlock_init(rwlock_t *rw)
+{
+    rw->readers = 0;
+    Sem_init(&rw->lock, 1);
+    Sem_init(&rw->writelock, 1);
 }
 
-void rwlock_acquire_readlock(rwlock_t *rw) {
+void rwlock_acquire_readlock(rwlock_t *rw)
+{
+    Sem_wait(&rw->lock);
+    rw->readers++;
+
+    if (rw->readers == 1)
+        Sem_wait(&rw->writelock);
+
+    Sem_post(&rw->lock);
 }
 
-void rwlock_release_readlock(rwlock_t *rw) {
+void rwlock_release_readlock(rwlock_t *rw)
+{
+    Sem_wait(&rw->lock);
+    rw->readers--;
+
+    if (rw->readers == 0)
+        Sem_post(&rw->writelock);
+
+    Sem_post(&rw->lock);
 }
 
-void rwlock_acquire_writelock(rwlock_t *rw) {
+void rwlock_acquire_writelock(rwlock_t *rw)
+{
+    Sem_wait(&rw->lock);
+    Sem_wait(&rw->writelock);
 }
 
-void rwlock_release_writelock(rwlock_t *rw) {
+void rwlock_release_writelock(rwlock_t *rw)
+{
+    Sem_post(&rw->lock);
+    Sem_post(&rw->writelock);
 }
 
 //
 // Don't change the code below (just use it!)
-// 
+//
 
 int loops;
 int value = 0;
 
 rwlock_t lock;
 
-void *reader(void *arg) {
+void *reader(void *arg)
+{
     int i;
-    for (i = 0; i < loops; i++) {
-	rwlock_acquire_readlock(&lock);
-	printf("read %d\n", value);
-	rwlock_release_readlock(&lock);
+    for (i = 0; i < loops; i++)
+    {
+        rwlock_acquire_readlock(&lock);
+        printf("read %d\n", value);
+        rwlock_release_readlock(&lock);
     }
     return NULL;
 }
 
-void *writer(void *arg) {
+void *writer(void *arg)
+{
     int i;
-    for (i = 0; i < loops; i++) {
-	rwlock_acquire_writelock(&lock);
-	value++;
-	printf("write %d\n", value);
-	rwlock_release_writelock(&lock);
+    for (i = 0; i < loops; i++)
+    {
+        rwlock_acquire_writelock(&lock);
+        value++;
+        printf("write %d\n", value);
+        rwlock_release_writelock(&lock);
     }
     return NULL;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     assert(argc == 4);
     int num_readers = atoi(argv[1]);
     int num_writers = atoi(argv[2]);
@@ -70,17 +105,16 @@ int main(int argc, char *argv[]) {
 
     int i;
     for (i = 0; i < num_readers; i++)
-	Pthread_create(&pr[i], NULL, reader, NULL);
+        Pthread_create(&pr[i], NULL, reader, NULL);
     for (i = 0; i < num_writers; i++)
-	Pthread_create(&pw[i], NULL, writer, NULL);
+        Pthread_create(&pw[i], NULL, writer, NULL);
 
     for (i = 0; i < num_readers; i++)
-	Pthread_join(pr[i], NULL);
+        Pthread_join(pr[i], NULL);
     for (i = 0; i < num_writers; i++)
-	Pthread_join(pw[i], NULL);
+        Pthread_join(pw[i], NULL);
 
     printf("end: value %d\n", value);
 
     return 0;
 }
-
